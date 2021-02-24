625aeec39c8bba6963db29dfe87e3d0b00c55279
blu-men <cjbb5eyv@icloud.com>
2021-02-24 22:24:54 +0900

    placed_testfiles

---
+++ tests/TestCase/Model/Entity/TaskTest.php
@@ -0,0 +1,52 @@
+<?php
+namespace App\Test\TestCase\Model\Entity;
+
+use App\Model\Entity\Task;
+use Cake\TestSuite\TestCase;
+
+/**
+ * App\Model\Entity\Task Test Case
+ */
+class TaskTest extends TestCase
+{
+
+    /**
+     * Test subject
+     *
+     * @var \App\Model\Entity\Task
+     */
+    public $Task;
+
+    /**
+     * setUp method
+     *
+     * @return void
+     */
+    public function setUp() :void
+    {
+        parent::setUp();
+        $this->Task = new Task();
+    }
+
+    /**
+     * tearDown method
+     *
+     * @return void
+     */
+    public function tearDown() :void
+    {
+        unset($this->Task);
+
+        parent::tearDown();
+    }
+
+    /**
+     * Test initial setup
+     *
+     * @return void
+     */
+    public function testInitialization()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+}
