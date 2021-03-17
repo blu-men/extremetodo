de7df3c415348b1f6ca9005ce3c311a856e2d1b1
blu-men <cjbb5eyv@icloud.com>
2021-03-18 04:17:25 +0900

    delete_auth Please enter the commit message for your changes. Lines starting

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -106,19 +106,19 @@ class UsersController extends AppController
  
     #以下のメソッドを追加
  
-    public function login()
-    {
-        if ($this->request->is('post')) {
-            $user = $this->Auth->identify();
-            if ($user) {
-                $this->Auth->setUser($user);
-                return $this->redirect($this->Auth->redirectUrl());
-            }
-            $this->Flash->error(__('Invalid username or password, try again'));
-        }
-    }
-    public function logout()
-    {
-        return $this->redirect($this->Auth->logout());
-    }
+    // public function login()
+    // {
+    //     if ($this->request->is('post')) {
+    //         $user = $this->Auth->identify();
+    //         if ($user) {
+    //             $this->Auth->setUser($user);
+    //             return $this->redirect($this->Auth->redirectUrl());
+    //         }
+    //         $this->Flash->error(__('Invalid username or password, try again'));
+    //     }
+    // }
+    // public function logout()
+    // {
+    //     return $this->redirect($this->Auth->logout());
+    // }
 }
